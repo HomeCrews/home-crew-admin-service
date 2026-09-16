2ac35be69a4ac85d2a33fa6cd9bbff2dc7a65aa6
mThanuj <mullagurithanuj0@gmail.com>
2026-09-16 14:51:33 +0530

    fix: formatted

--- src/main/java/com/homecrew/adminservice/controller/AdminController.java
+++ src/main/java/com/homecrew/adminservice/controller/AdminController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin")
 public class AdminController {
 
-  @GetMapping("/test")
-  public String test() {
-    return "Admin Service is working";
-  }
+    @GetMapping("/test")
+    public String test() {
+        return "Admin Service is working";
+    }
 }
