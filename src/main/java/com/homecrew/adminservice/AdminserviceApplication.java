48bb7277f85e9b405e02de741012c712b458eb16
mThanuj <mullagurithanuj0@gmail.com>
2026-09-16 14:51:33 +0530

    fix: formatted

--- src/main/java/com/homecrew/adminservice/AdminserviceApplication.java
+++ src/main/java/com/homecrew/adminservice/AdminserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AdminserviceApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(AdminserviceApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(AdminserviceApplication.class, args);
+    }
 }
